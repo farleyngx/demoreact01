2552fea718fed41176482104c7db7cea47337835
FarleyNGX <minhthanh2012005@gmail.com>
2025-09-16 11:31:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav class="navbar">
+        <ul className="left-nav d-flex">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="main-content container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <div>
+                <img
+                  className="img-fluid"
+                  src="src\assets\portugal_v_slovakia_group_j_-_uefa_euro_2024_european_qualifiers.jpeg"
+                  alt=""
+                />
+                <h6 className="card-footer text-center">Cristiano Ronaldo</h6>
+                <p className="text-center">MU</p>
+                <h6 className="card-footer text-center detail">Details</h6>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <div>
+                <img
+                  className="img-fluid"
+                  src="src\assets\portugal_v_slovakia_group_j_-_uefa_euro_2024_european_qualifiers.jpeg"
+                  alt=""
+                />
+                <h6 className="card-footer text-center">Cristiano Ronaldo</h6>
+                <p className="text-center">MU</p>
+                <h6 className="card-footer text-center detail">Details</h6>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <div>
+                <img
+                  className="img-fluid"
+                  src="src\assets\portugal_v_slovakia_group_j_-_uefa_euro_2024_european_qualifiers.jpeg"
+                  alt=""
+                />
+                <h6 className="card-footer text-center">Cristiano Ronaldo</h6>
+                <p className="text-center">MU</p>
+                <h6 className="card-footer text-center detail">Details</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <div>
+                <img
+                  className="img-fluid"
+                  src="src\assets\portugal_v_slovakia_group_j_-_uefa_euro_2024_european_qualifiers.jpeg"
+                  alt=""
+                />
+                <h6 className="card-footer text-center">Cristiano Ronaldo</h6>
+                <p className="text-center">MU</p>
+                <h6 className="card-footer text-center detail">Details</h6>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <div>
+                <img
+                  className="img-fluid"
+                  src="src\assets\portugal_v_slovakia_group_j_-_uefa_euro_2024_european_qualifiers.jpeg"
+                  alt=""
+                />
+                <h6 className="card-footer text-center">Cristiano Ronaldo</h6>
+                <p className="text-center">MU</p>
+                <h6 className="card-footer text-center detail">Details</h6>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <div>
+                <img
+                  className="img-fluid"
+                  src="src\assets\portugal_v_slovakia_group_j_-_uefa_euro_2024_european_qualifiers.jpeg"
+                  alt=""
+                />
+                <h6 className="card-footer text-center">Cristiano Ronaldo</h6>
+                <p className="text-center">MU</p>
+                <h6 className="card-footer text-center detail">Details</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer className="footer text-center mt-4 fixed-bottom">
+        <p>copyright</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
