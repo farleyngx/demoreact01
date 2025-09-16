8c197be73c72babdd8ec4e53da5f0fb9243f7914
FarleyNGX <minhthanh2012005@gmail.com>
2025-09-16 14:17:01 +0700

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./App.css";
-import Content from "./components/Content";
+import React from "react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import Player from "./components/Player";
+import "./App.css";
+
 
 function App() {
   return (
     <>
       <NavBar />
-      <Content />
+      <Player />
       <Footer />
     </>
   );
